6621d6788ced66ea96f2522bc89dc34ead70f0be
maralimjanv <umarolimjonov2009@gmail.com>
2024-12-03 16:46:24 +0500

    crud pull

--- src/books/urls.py
+++ src/books/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from .views import BooksAPIView, AuthorRetrieveAPIView
-
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('books', BooksAPIView.as_view(), name='booksold-list-create'),
     path('books/<int:pk>', BooksAPIView.as_view(), name='delete-update'),
     path('authors/<int:pk>', AuthorRetrieveAPIView.as_view(), name='author-detail'),
+
 ]
+
+
+
+
+
 
 
